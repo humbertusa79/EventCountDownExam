7ee07b2e1919be69eb8d0c3f371e23f33724c6dc
Humberto Partida <humbertusa79@gmail.com>
2025-05-26 15:08:14 -0700

    chore: initial commit, project creation

---
+++ EventCountDown/EventCountDown/EventCountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountDownApp.swift
+//  EventCountDown
+//
+//  Created by HumbertoPartida on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct EventCountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
